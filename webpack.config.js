493796dcd27e2e59b17815b0ba3e6908d2e01449
Ernesto Bellei <01jam@hund.studio>
2021-12-28 15:51:06 +0100

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+	mode: "production",
+	entry: { index: "./src/index.ts" },
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		path: path.resolve(__dirname, "lib/umd"),
+		filename: "[name].js",
+		libraryTarget: "umd",
+		library: "MyLib",
+		umdNamedDefine: true,
+	},
+};
